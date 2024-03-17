113377396d8c7ae9ceeac19ef9134d6d785fe30e
vanshika704 <sharmavanshi704@gmail.com>
2024-03-18 00:05:37 +0530

    commit

---
+++ .vscode/javascript/1.js
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<h2>JavaScript beauty</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Coding is fun until you get a stupid error to work upon for hours -_-"'>programmers thought </button>
+
+</body>
+</html>
