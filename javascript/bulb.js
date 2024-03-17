8167c6bcae6a4acbcbe67b6aa9166b547a81d7f6
vanshika704 <sharmavanshi704@gmail.com>
2024-03-18 00:32:02 +0530

    commit

---
+++ javascript/bulb.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Turn on the bulb</title>
+    </head>
+    <body>
+        <button onclick="document.getElementById('myImage').src='bulbon.png'">Turn on the light</button>
+        <img id="myImage" src="bulbon.png" style="width: 100px;">
+        <button onclick="document.getElementById('myImage').src='bulboff.png'">Turn off the light</button>
+    </body>
+</html>
