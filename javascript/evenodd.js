8ace3a5e27aa6a3b0010b5a97d31440784f7b4b9
Vanshika Sharma <143260941+vanshika704@users.noreply.github.com>
2024-04-28 16:21:24 +0530

    Update evenodd.js

--- javascript/evenodd.js
+++ javascript/evenodd.js
@@ -1,6 +1,6 @@
 var userInput = prompt("Enter a number:");
 var number = parseInt(userInput);
-if(number%2==0){
+if(number%2==0){// if any number modulus by 2 is zero, it is even 
     console.log("the number is even");
 }
 else{
